616b2b3e617c4c6364b5b5c41631ae28f27c812b
Spyros Argalias <sargalias@gmail.com>
2023-09-24 16:02:03 +0100

    add cdk step function

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stateMachine = new sfn.StateMachine(
+      this,
+      'CdkHelloWorldStateMachine',
+      {
+        definitionBody: sfn.DefinitionBody.fromFile(
+          './lib/helloWorldStepFunction.json',
+        ),
+      },
+    );
   }
 }
