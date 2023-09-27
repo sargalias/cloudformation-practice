2d4d5f05c1dd0bee09a6c47c2440bd778cbecbd4
Spyros Argalias <sargalias@gmail.com>
2023-09-27 20:55:34 +0100

    add terraform s3 back end example, create resources

---
+++ packages/terraform-practice/s3-backend/main.tf
@@ -0,0 +1,55 @@
+variable "global_tags" {
+  type = map(string)
+  default = {
+    foo = "terraform"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  # backend "s3" {
+  #   bucket         = "sargalias-terraform-state"
+  #   key            = "terraform.tfstate"
+  #   region         = "eu-west-2"
+  #   encrypt        = true
+  #   dynamodb_table = "terraform_locks"
+  # }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "sargalias-terraform-state"
+  force_destroy = true
+  tags          = merge(var.global_tags)
+}
+
+resource "aws_s3_bucket_versioning" "versioning_terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "terraform_locks"
+  hash_key       = "LockID"
+  read_capacity  = 1
+  write_capacity = 1
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = merge(var.global_tags)
+}
+
+
